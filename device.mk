6bfff8a8e10d4c7fb9d2bc76e216f7468cfb288f
Dan Elkouby <streetwalrus@codewalr.us>
2016-09-28 09:06:39 +0300

    op2: device.mk: remove telephony-ext [1/3]
    It is now included globally in vendor/cm and prevents boot.art from
    being compiled
    
    Change-Id: Ib53250b633cfe32587ef15720e900b51d55a2377

--- device.mk
+++ device.mk
@@ -231,9 +231,6 @@ PRODUCT_PACKAGES += \
     librmnetctl \
     libxml2
 
-PRODUCT_PACKAGES += telephony-ext
-PRODUCT_BOOT_JARS += telephony-ext
-
 # Sensors
 PRODUCT_PACKAGES += \
     sensors.msm8994 \
