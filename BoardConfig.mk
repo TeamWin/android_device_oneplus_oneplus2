a823dd9cb75321b62d5b3061dbad5c552eb1beee
www.anuprita804@gmail.com <www.anuprita804@gmail.com>
2017-03-23 21:51:50 +0100

    op2: rebrand to haruhios check this: https://github.com/anupritaisno1/android_kernel_oneplus_msm8994/commit/f8b2fc1de45e041dc6da91157052107d131e9308

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -70,7 +70,7 @@ TARGET_KERNEL_APPEND_DTB := true
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/oneplus/msm8994
-TARGET_KERNEL_CONFIG := oneplus2_defconfig
+TARGET_KERNEL_CONFIG := haruhi_defconfig
 TARGET_KERNEL_CROSS_COMPILE_PREFIX := aarch64-linux-android-
 
 # QCOM hardware
