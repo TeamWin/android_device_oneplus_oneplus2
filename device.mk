00a5e982b0866f9ab9e216a19482eec1d7feaec0
anupritaisno1 <www.anuprita804@gmail.com>
2017-11-27 17:23:16 +0100

    op2: doze: twrp: nuke opdoze into oblivion twrp doesn't need this
    Signed-off-by: anupritaisno1 <www.anuprita804@gmail.com>

--- device.mk
+++ device.mk
@@ -125,10 +125,6 @@ PRODUCT_PACKAGES += \
     liboverlay \
     libtinyxml
 
-# Doze mode
-PRODUCT_PACKAGES += \
-    OneplusDoze
-
 # Filesystem management tools
 PRODUCT_PACKAGES += \
     e2fsck \
